158ad96917ea3395e28a7bcc688046d8c469a178
viktoriia-zubanenko-inhale-tech <151815588+viktoriia-zubanenko-inhale-tech@users.noreply.github.com>
2024-01-08 21:04:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,29 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { createDiscordClient, createMessedge } = require("./functions/discord/discord");
 const { youtubeUpload } = require("./functions/other requests/helperFunctions");
+const { addCommentToJira } = require("./functions/other requests/otherRequests");
 require("dotenv").config();
 
 app.listen(port, process.env.HOSTNAME, async () => {
   const dsclient = createDiscordClient();
-  dsclient.on("messageCreate", createMessedge);
-  await dsclient.login(process.env.BOT_TOKEN);
+
+  dsclient.on("messageCreate", async function (message) {
+    await new Promise((r) => setTimeout(r, 1000));
+    const guild = dsclient.guilds.cache.get(message.guildId);
+    const threads = guild.channels.cache
+      .sort((a, b) => a.createdAt - b.createdAt)
+      .filter((x) => x.isThread());
+    let thread = threads.find((x) => x.id == message.channelId);
+    if (thread !== "undefined" && thread.name !== "undefined" && message.author.bot !== true) {
+      await addCommentToJira(thread.name, message.content, message.author.username);
+    }
+  });
+
+  dsclient.login(process.env.BOT_TOKEN);
 
   let intervalId = 0;
   clearInterval(intervalId);
+
   await youtubeUpload();
   intervalId = setInterval(youtubeUpload, process.env.INTERVAL_MILLISECONDS);
   intervalId = 0;
