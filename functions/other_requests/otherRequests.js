5cd28c678d026674219119a9f2da3b2b41ccdf82
viktoriia-zubanenko-inhale-tech <151815588+viktoriia-zubanenko-inhale-tech@users.noreply.github.com>
2024-01-09 10:09:07 +0200

    Add files via upload

---
+++ functions/other_requests/otherRequests.js
@@ -0,0 +1,64 @@
+const fetch = require("node-fetch");
+const axios = require("axios");
+const Version3Client = require("jira.js").Version3Client;
+require("dotenv").config();
+
+async function ragicGetProjectFoldersRequest() {
+  let response = [];
+  const headers = {
+    Authorization: `${process.env.RAGIC_AUTH}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const responseRagic = await axios.get(process.env.RAGIC_REQUEST_URL, { headers });
+    if (responseRagic.status !== 200) {
+      console.error(`Request failed with status code: ${responseRagic.status}`);
+      return response;
+    }
+    let responceData = responseRagic.data;
+    for (const Key in responceData) {
+      if (typeof responceData[Key] != "object") continue;
+      let iteratableObject = responceData[Key];
+      let dataObject = {};
+      dataObject[process.env.FOLDER_TYPE_PUBLIC] = iteratableObject[process.env.PUBLIC_DRIVE];
+      dataObject[process.env.FOLDER_TYPE_PRIVATE] = iteratableObject[process.env.PRIVATE_DRIVE];
+      dataObject[process.env.FOLDER_TYPE_INTERNAL] = iteratableObject[process.env.INTERNAL_DRIVE];
+      dataObject[process.env.PLAYLIST] = iteratableObject[process.env.PLAYLIST];
+      dataObject[process.env.PROJECT_NAME] = iteratableObject[process.env.PROJECT_NAME];
+      dataObject[process.env.RAGIC_ID] = parseInt(Key);
+      response.push(dataObject);
+      dataObject = {};
+    }
+  } catch (error) {
+    console.error("ragicGetProjectFoldersRequest: unexpected error during ragic request:", error.message);
+  }
+  return response;
+}
+
+async function makeSendPostRequest(requestBody) {
+  let response = false;
+  try {
+    await axios.post(process.env.RESPONSE_WEBHOOK_URL, requestBody);
+    response = true;
+  } catch (error) {
+    console.error("makeSendPostRequest: unexpected error happend:", error.message);
+  }
+  return response;
+}
+
+async function addCommentToJira(threadName, comment, author) {
+  let reqBody = {
+    host: process.env.JIRA_HOST,
+    authentication: {
+      basic: {
+        email: process.env.ROOT_EMAIL,
+        apiToken: process.env.API_KEY,
+      },
+    },
+  };
+
+  const client = new Version3Client(reqBody);
+  await fetch(`${process.env.JIRA_COMMENT_URL}?issueId=${threadName}&comment=${comment}&author=${author}`);
+}
+
+module.exports = { ragicGetProjectFoldersRequest, makeSendPostRequest, addCommentToJira };
