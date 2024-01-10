09f44d377cea021a26c456422a806448f08523b8
viktoriia-zubanenko-inhale-tech <151815588+viktoriia-zubanenko-inhale-tech@users.noreply.github.com>
2024-01-10 10:19:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 const { createDiscordClient, createMessedge } = require("./functions/discord/discord");
 const { youtubeUpload } = require("./functions/other_requests/helperFunctions");
-const { addCommentToJira } = require("./functions/other_requests/otherRequests");
 const Discord = require("discord.js");
 const { GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
@@ -15,15 +14,7 @@ app.listen(port, process.env.HOSTNAME, async () => {
   const dsclient = new Discord.Client(body);
 
   dsclient.on("messageCreate", async function (message) {
-    await new Promise((r) => setTimeout(r, 1000));
-    const guild = dsclient.guilds.cache.get(message.guildId);
-    const threads = guild.channels.cache
-      .sort((a, b) => a.createdAt - b.createdAt)
-      .filter((x) => x.isThread());
-    let thread = threads.find((x) => x.id == message.channelId);
-    if (thread !== "undefined" && thread.name !== "undefined" && message.author.bot !== true) {
-      await addCommentToJira(thread.name, message.content, message.author.username);
-    }
+    await createMessedge(message, dsclient);
   });
 
   dsclient.login(process.env.BOT_TOKEN);
