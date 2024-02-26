ced7f573fdc946ca776ca0c87713cccb8a0dbbcc
viktoriia-zubanenko-inhale-tech <151815588+viktoriia-zubanenko-inhale-tech@users.noreply.github.com>
2024-02-26 09:34:57 +0200

    Add files via upload

---
+++ changePermission/client/src/App.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
+import axios from 'axios';
+import './App.css'
+
+const App = () => {
+  const [inputValue, setInputValue] = useState('');
+  const handleSubmit = async (e) => {
+   // const navigate = useNavigate(); 
+    e.preventDefault();
+
+    try {
+      const url = 'http://localhost:3000/authorization'; 
+      const body = {
+        params: {
+          userName: inputValue
+        }
+      };
+      axios.get(url,body).then(res=>{
+        console.log(res);
+        if (res.status == 200) {
+          window.location.replace(res.data.url_redirect);
+   //       navigate('/another-page');
+        } else {
+          console.error('Error: response bode absent', res);
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+
+    }
+  };
+
+  return (
+    <div>
+      <header>
+       <h1>User transfer ownersip form</h1>
+      </header>
+      <main>
+        <form className='frm-sbmt' onSubmit={handleSubmit}>
+          <h2 className='hddr-sbmt'>File Ownership transfer</h2>
+          <div>
+            <label htmlFor="inputField">Your full name: </label>
+
+            <input
+              className='inpt-sbmt'
+              type="text"
+              id="inputField"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </div>
+          <div>
+          <button type="submit" className='bttn-sbmt'>Submit</button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+
+export default App;
